7d36b59a9973c2bdbf3b6d6155b9e0186280e8d2
Floris Erich <floris.erich@aist.go.jp>
2018-04-27 15:16:05 +0900

    Moved main into seperate file to support testing. Added a file for adding tests.

--- src/rsj_2017_pick_and_placer/src/pick_and_placer.cpp
+++ src/rsj_2017_pick_and_placer/src/pick_and_placer.cpp
@@ -290,20 +290,3 @@ class PickNPlacer {
   float gripper_open_;
   float gripper_close_;
 };
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "pickandplacer");
-
-  ros::AsyncSpinner spinner(2);
-  spinner.start();
-
-  ros::NodeHandle nh;
-  // Create an instance of the class that implements the node's behaviour
-  PickNPlacer pnp(nh);
-
-  // Wait until the node is shut down
-  ros::waitForShutdown();
-
-  ros::shutdown();
-  return 0;
-}
