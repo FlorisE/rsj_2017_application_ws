082432420153a4c758294632903ab12efbcc062b
Floris Erich <floris.erich@aist.go.jp>
2018-05-02 15:29:22 +0900

    More refactoring

--- src/rsj_2017_pick_and_placer/src/pick_and_placer_node.cpp
+++ src/rsj_2017_pick_and_placer/src/pick_and_placer_node.cpp
@@ -1,5 +1,28 @@
 #include <ros/ros.h>
+
+#include "gripper.h"
 #include "pick_and_placer.cpp"
+
+void GetROSParams(PickNPlacerParams& params) {
+  // Get the value for the configurable values from the parameter server, and
+  // set sensible defaults for those values not specified on the parameter
+  // server
+  ros::param::param<float>(
+    "~place_x",
+    params.place_x_,
+    0.1);
+  ros::param::param<float>("~place_y", params.place_y_, -0.2);
+  ros::param::param<std::__cxx11::string>(
+    "~task_frame",
+    params.scene_task_frame_,
+    "base_link");
+  ros::param::param<float>("~pick_prepare_z", params.pick_prepare_z_, 0.1);
+  ros::param::param<float>("~pick_z", params.pick_z_, 0.05);
+  ros::param::param<float>("~place_prepare_z", params.place_prepare_z_, 0.1);
+  ros::param::param<float>("~place_z", params.place_z_, 0.05);
+  ros::param::param<float>("~gripper_open", params.gripper_open_, 0.1);
+  ros::param::param<float>("~gripper_close", params.gripper_close_, 0.015);
+}
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "pickandplacer");
@@ -9,7 +32,12 @@ int main(int argc, char **argv) {
 
   ros::NodeHandle nh;
   // Create an instance of the class that implements the node's behaviour
-  PickNPlacer pnp(nh);
+  PickNPlacerParams params;
+  GetROSParams(params);
+
+  Gripper gripper("/crane_plus_gripper/gripper_command", "true");
+
+  PickNPlacer pnp(nh, gripper, params);
 
   // Wait until the node is shut down
   ros::waitForShutdown();
