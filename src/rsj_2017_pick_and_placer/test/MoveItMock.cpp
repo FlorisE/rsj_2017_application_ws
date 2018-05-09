2864a52b590c012cec34ad22c599d094634a5ed9
Floris Erich <floris.erich@aist.go.jp>
2018-05-09 11:03:55 +0900

    Added tests for Arm::DoPick, solved some minor issues with the implementation

--- src/rsj_2017_pick_and_placer/test/MoveItMock.cpp
+++ src/rsj_2017_pick_and_placer/test/MoveItMock.cpp
@@ -1,5 +1,6 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+const std::vector<std::string> linkNames;
 
 namespace moveit {
   namespace planning_interface {
@@ -9,5 +10,19 @@ namespace moveit {
     void MoveGroupInterface::setPoseReferenceFrame(const std::string& surfaceName) {}
     MoveGroupInterface::~MoveGroupInterface() {}
     PlanningSceneInterface::PlanningSceneInterface() {}
+    bool MoveGroupInterface::setPoseTargets(const std::vector<geometry_msgs::PoseStamped>& target, 
+                                            const std::string& end_effector_link) {
+      return true;
+    }
+    MoveItErrorCode MoveGroupInterface::move() {
+      return MoveItErrorCode(moveit_msgs::MoveItErrorCodes::SUCCESS);
+    }
+    const std::vector<std::string>& MoveGroupInterface::getLinkNames() {
+      return ::linkNames;
+    }
+    bool MoveGroupInterface::attachObject(const std::string& object, const std::string& link, 
+                                          const std::vector<std::string>& touch_links) {
+      return true;
+    }
   }
 }
