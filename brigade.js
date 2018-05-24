9ae799354cd3d6ee4edcb307f17f636c6e8502b5
Floris Erich <floris.erich@aist.go.jp>
2018-05-24 19:22:22 +0900

    Brigade gets ROS and installs moveit

--- brigade.js
+++ brigade.js
@@ -1,9 +1,15 @@
 const { events, Job } = require("brigadier")
 
+console.log("running brigade")
+
 events.on("push", function(e, project) {
-  var job = new Job("echo-in-container", "kinetic-ros-base")
+  var job = new Job("build", "ros:kinetic-robot-xenial")
   job.tasks = [
-    "echo Test"
+    "apt-get update",
+    "apt-get install -y ros-kinetic-moveit-*",
+    "/bin/bash",
+    ". /opt/ros/kinetic/setup.sh",
+    "cd /src"
   ]
 
   job.run()
