91c3f092fc110d4891a5f09280e3881e9ef10e07
FlorisE <florisjuh@gmail.com>
2018-05-25 13:47:52 +0900

    Setting up brigade (#5)
    * Forgot to import Job
    
    * Change to trigger build
    
    * Brigade gets ROS and installs moveit
    
    * Fixed brigade building

--- brigade.js
+++ brigade.js
@@ -1,10 +1,24 @@
-const { events, Job } = require("brigadier")
+const { events, Job, Group } = require("brigadier")
+
+const image = "rtmaist.azurecr.io/ros-robot-xenial-moveit:latest"
 
 events.on("push", function(e, project) {
-  var job = new Job("echo-in-container", "kinetic-ros-base")
-  job.tasks = [
-    "echo Test"
+  var getImage = new Job("get-image", image)
+  getImage.imageForcePull = true
+
+  var build = new Job("build", image)
+  build.tasks = [
+    ". /opt/ros/kinetic/setup.sh",
+    "cd /src",
+    "catkin_make"
   ]
 
-  job.run()
+  var test = new Job("test", image)
+  test.tasks = [
+    ". /opt/ros/kinetic/setup.sh",
+    "cd /src",
+    "catkin_make run_tests"
+  ]
+
+  Group.runEach([getImage, build, test])
 })
